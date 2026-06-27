0e27237ad121a58b1d8a0c26d754aa6f23b238bd
Matheus Fernandes <14061352+MatheusEstatistico@users.noreply.github.com>
2026-06-27 14:18:49 -0300

    modelo de arvore

--- 1. Titanic.py
+++ 1. Titanic.py
@@ -19,3 +19,20 @@ rate_men = sum(men)/len(men)
 
 print("% of men who survived:", rate_men)
 
+## Modelo 1:  Random Forest (modelo de classificação)
+
+from sklearn.ensemble import RandomForestClassifier
+
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('submission.csv', index=False)
+print("Your submission was successfully saved!")
